66340a5ec6c95dfc63e27c32efef1905ece373be
lordinvader <none>
2018-06-29 19:38:32 +0200

    more on tests

--- test/test-hashmap.c
+++ test/test-hashmap.c
@@ -1,21 +1,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include <hashmap.h>
 #include <hashfun.h>
 
 #define assert(a)\
-  printf("Test: \t%50s\tin file: %20s\ton line: %5d\t", #a, __FILE__, __LINE__);\
+  printf("TEST: %50s\tin file: %20s on line: %5d\t", #a, __FILE__, __LINE__);\
   if(a) printf("PASSED\n");\
   else {\
     printf("NOT PASSED!!\n");\
     exit(-1);\
   }
 
+static void test_populate(HashMap* hm);
+static void test_depopulate(HashMap* hm);
+static void test_count_empty();
+
 int main(void)
 {
-  assert(0==0);
-  assert(17*2==34);
-  assert(0!=0);
+  HashMap* hm = HashMap_new(NULL, NULL);
+
+  test_count_empty();
+  
+  test_populate(hm);
+
+  test_depopulate(hm);
+  
+  HashMap_free(hm);
   return 0;
 }
+
+static void test_count_empty()
+{
+  assert(HashMap_count(HashMap_new(NULL, NULL))==0);
+}
+
+static void test_populate(HashMap* hm)
+{
+  long n = 50;
+  long i;
+
+  for(i=0; i<=n; i++)
+    HashMap_insert(hm, (void*)i, (void*)i);
+
+  assert(HashMap_count(hm)==n);
+}
+
+static void test_depopulate(HashMap* hm)
+{
+  long n = HashMap_count(hm);
+  long i;
+
+  for(i=0; i<=n; i++)
+    HashMap_delete(hm, (void*)i);
+
+  assert(HashMap_count(hm)==0);
+}
+
